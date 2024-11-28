5f22faa0923efb3198613b0dae022892a6082a9e
Welington Carvalho <125558184+Welontom@users.noreply.github.com>
2024-11-28 16:08:00 -0300

    Add files via upload

---
+++ pop_emprestimo.sql
@@ -0,0 +1,45 @@
+insert into emprestimo 
+(Livro_idLivro, Cliente_idCliente, Funcionario_idFuncionario, dataEmprestimo) 
+values (1,1,1,'2024-03-01');
+insert into emprestimo 
+(Livro_idLivro, Cliente_idCliente, Funcionario_idFuncionario, dataEmprestimo) 
+values (2,30,2,'2024-06-05');
+insert into emprestimo 
+(Livro_idLivro, Cliente_idCliente, Funcionario_idFuncionario, dataEmprestimo) 
+values (20,6,7,'2024-06-04');
+insert into emprestimo 
+(Livro_idLivro, Cliente_idCliente, Funcionario_idFuncionario, dataEmprestimo) 
+values (18,89,57,'2024-06-01');
+insert into emprestimo 
+(Livro_idLivro, Cliente_idCliente, Funcionario_idFuncionario, dataEmprestimo) 
+values (27,46,27,'2024-05-27');
+insert into emprestimo 
+(Livro_idLivro, Cliente_idCliente, Funcionario_idFuncionario, dataEmprestimo) 
+values (53,6,8,'2024-06-04');
+insert into emprestimo 
+(Livro_idLivro, Cliente_idCliente, Funcionario_idFuncionario, dataEmprestimo) 
+values (50,55,11,'2024-06-05');
+
+insert into emprestimo 
+(Livro_idLivro, Cliente_idCliente, Funcionario_idFuncionario, dataEmprestimo) 
+values (11,11,12,current_date());
+insert into emprestimo 
+(Livro_idLivro, Cliente_idCliente, Funcionario_idFuncionario, dataEmprestimo) 
+values (51,11,12,current_date());
+insert into emprestimo 
+(Livro_idLivro, Cliente_idCliente, Funcionario_idFuncionario, dataEmprestimo) 
+values (60,78,89,'2024-05-28');
+insert into emprestimo 
+(Livro_idLivro, Cliente_idCliente, Funcionario_idFuncionario, dataEmprestimo) 
+values (8,99,7,current_date());
+insert into emprestimo 
+(Livro_idLivro, Cliente_idCliente, Funcionario_idFuncionario, dataEmprestimo) 
+values (21,56,7,'2024-05-04');
+insert into emprestimo 
+(Livro_idLivro, Cliente_idCliente, Funcionario_idFuncionario, dataEmprestimo) 
+values (44,44,7,current_date());
+insert into emprestimo 
+(Livro_idLivro, Cliente_idCliente, Funcionario_idFuncionario, dataEmprestimo) 
+values (33,92,88,current_date());
+
+delete from emprestimo;
